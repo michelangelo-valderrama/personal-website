32caaf7e23e6a1d22327d7cc6d65ae6a0fa3627b
michelangelo-valderrama <michelangelovalderrama@gmail.com>
2024-02-01 20:26:11 +0100

    robots y sitemap

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,59 +1,46 @@
-import { defineConfig } from "astro/config"
+import { defineConfig } from "astro/config";
 /* integrations */
-import tailwind from "@astrojs/tailwind"
-import react from "@astrojs/react"
-import mdx from "@astrojs/mdx"
-import expressiveCode from "astro-expressive-code"
+import tailwind from "@astrojs/tailwind";
+import react from "@astrojs/react";
+import mdx from "@astrojs/mdx";
+import expressiveCode from "astro-expressive-code";
 /* plugins */
-import { rehypeHeadingIds } from "@astrojs/markdown-remark"
-import { remarkImageOptimization, remarkReadingTime, rehypeLinkHeading } from "./plugins/index"
-import remarkImgAttr from "remark-imgattr"
-import remarkMath from "remark-math"
-import remarkToc from "remark-toc"
-import rehypeKatex from "rehype-katex"
+import { rehypeHeadingIds } from "@astrojs/markdown-remark";
+import { remarkImageOptimization, remarkReadingTime, rehypeLinkHeading } from "./plugins/index";
+import remarkImgAttr from "remark-imgattr";
+import remarkMath from "remark-math";
+import remarkToc from "remark-toc";
+import rehypeKatex from "rehype-katex";
 /* others */
-import theme from "./public/vercel-theme.json"
+import theme from "./public/vercel-theme.json";
+import sitemap from "@astrojs/sitemap";
+const site = "http://localhost:4321/";
 
-const site = "http://localhost:4321/"
 
 // https://astro.build/config
 export default defineConfig({
   site,
   markdown: {
-    remarkPlugins: [
-      remarkToc,
-      remarkMath,
-      remarkImgAttr,
-      [remarkImageOptimization, { figure: true }],
-      remarkReadingTime,
-    ],
-    rehypePlugins: [
-      rehypeHeadingIds,
-      rehypeLinkHeading,
-      rehypeKatex
-    ],
+    remarkPlugins: [remarkToc, remarkMath, remarkImgAttr, [remarkImageOptimization, {
+      figure: true
+    }], remarkReadingTime],
+    rehypePlugins: [rehypeHeadingIds, rehypeLinkHeading, rehypeKatex]
   },
-  integrations: [
-    tailwind({
-      applyBaseStyles: false,
-    }),
-    react(),
-    expressiveCode({
-      themes: [theme],
-      styleOverrides: {
-        borderColor: "#27272a",
-        codeBackground: "#18181b",
-        codeFontFamily:
-          "Cascadia, ui-monospace, SFMono-Regular, Menlo, Monaco, Consolas, 'Liberation Mono', 'Courier New', monospace",
-        codePaddingInline: "1rem",
-        frames: {
-          editorActiveTabBackground: "#18181b",
-          terminalBackground: "#18181b",
-          terminalTitlebarBackground: "#18181b",
-          shadowColor: "transparent",
-        },
-      },
-    }),
-    mdx(),
-  ],
-})
+  integrations: [tailwind({
+    applyBaseStyles: false
+  }), react(), expressiveCode({
+    themes: [theme],
+    styleOverrides: {
+      borderColor: "#27272a",
+      codeBackground: "#18181b",
+      codeFontFamily: "Cascadia, ui-monospace, SFMono-Regular, Menlo, Monaco, Consolas, 'Liberation Mono', 'Courier New', monospace",
+      codePaddingInline: "1rem",
+      frames: {
+        editorActiveTabBackground: "#18181b",
+        terminalBackground: "#18181b",
+        terminalTitlebarBackground: "#18181b",
+        shadowColor: "transparent"
+      }
+    }
+  }), mdx(), sitemap()]
+});
